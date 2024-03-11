c3ec61d63896f9eb8b90b9477d6d77ec1cbfd9fc
nachiket-01 <nachiketshinde2001@gmail.com>
2024-03-11 11:28:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import ProductsManagement from './components/ProductsManagement';
 import OrdersManagement from './components/OrdersManagement';
 import OrdersCalendar from './components/OrdersCalendar';
-import './App.css';
-
-// Custom component to redirect to homepage
-const RedirectToHomepage = () => {
-  useEffect(() => {
-    window.location.href = '/React-ERPSystem'; // Redirect to homepage
-  }, []);
-
-  return null;
-};
+import './App.css'; 
 
 const App = () => {
     return (
@@ -22,18 +14,17 @@ const App = () => {
                 <nav>
                     <ul>
                         <li><Link to="/React-ERPSystem">Dashboard</Link></li>
-                        <li><Link to="/React-ERPSystem/products">Manage Products</Link></li>
-                        <li><Link to="/React-ERPSystem/orders">Manage Orders</Link></li>
-                        <li><Link to="/React-ERPSystem/calendar">Orders Calendar</Link></li> 
+                        <li><Link to="/products">Manage Products</Link></li>
+                        <li><Link to="/orders">Manage Orders</Link></li>
+                        <li><Link to="/calendar">Orders Calendar</Link></li> 
                     </ul>
                 </nav>
 
                 <Routes>
-                    <Route exact path="/" element={<RedirectToHomepage />} />
-                    <Route path="/React-ERPSystem" element={<Dashboard />} />
-                    <Route path="/React-ERPSystem/products" element={<ProductsManagement />} />
-                    <Route path="/React-ERPSystem/orders" element={<OrdersManagement />} />
-                    <Route path="/React-ERPSystem/calendar" element={<OrdersCalendar />} />
+                    <Route exact path="/React-ERPSystem" element={<Dashboard />} />
+                    <Route path="/products" element={<ProductsManagement />} />
+                    <Route path="/orders" element={<OrdersManagement />} />
+                    <Route path="/calendar" element={<OrdersCalendar />} />
                 </Routes>
             </div>
         </Router>
